f5cfb319c06778cc9cf3fe533c170af6fadab2b7
lukas <lukas.manz03@gmail.com>
2024-12-11 21:53:50 +0100

    kleine änderung

--- backend/filterFile.py
+++ backend/filterFile.py
@@ -9,24 +9,26 @@ import os
 filter_result = {}
 
 def filter(graph, category, input):
+    print(len((graph)))
     print("categorie: " + str(category))
     print("input: " + str(input))
 
-    if category == 'focusNode':
+    if category == 'Violated FocusNodes':
         filterRes = filterNode(graph, input)
 
         print(filterRes)
-    if category == 'resultPath' :
+    if category == 'Violated ResultPaths' :
         newGraph = filterResultPath(graph, input)
         filter_result['lastAnalysis'] = analyze_graph(newGraph)
     if category == 'resultSeverity':
         newGraph = filterSeverity(graph, input)
         filter_result['lastAnalysis'] = analyze_graph(newGraph)
-    if category == 'filterSourceConstraintComponent':
+    if category == 'Violation Types':
         newGraph = filterSourceConstraintComponent(graph, input)
         filter_result['lastAnalysis'] = analyze_graph(newGraph)
     if category == 'sourceShape':
         newGraph = filterNode(graph, input)
         filter_result['lastAnalysis'] = analyze_graph(newGraph)
 
-    return jsonify({'status': 'successfull'})
+    return jsonify({'filter Ergebnis': filter_result})
+    
